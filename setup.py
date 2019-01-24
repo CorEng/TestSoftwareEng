e089fcd821969f146e8999d0578f7c2bbb0f0c6f
Cormac <cormacenglish@gmail.com>
2019-01-24 15:47:59 +0000

    fuck

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='Test2',
       description='Test module that returns system information',
       author='Cormac English',
       author_email='cormacenglish@gmail.com',
-      packages=['IdChecker']
+      packages=['Package']
      )
 
 #Checkin this works
